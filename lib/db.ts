6abd0a2833524ef3b6a9fc671ce643ad16cb248f
Maks Kolman <maks@kolman.si>
2021-12-08 23:47:17 +0100

    fix: close db after use

--- lib/db.ts
+++ lib/db.ts
@@ -36,6 +36,7 @@ async function save(ownerId: number, owner: string, event: string, data: string)
   const db = await openLeaderboardDB()
   const obj = {ownerId, owner, event, data, saveTime: Date.now()}
   await db.put(STORE_NAME, obj, makeKey(ownerId, event))
+  db.close()
 }
 
 async function remove(ownerId: number, event: string) {
@@ -43,6 +44,7 @@ async function remove(ownerId: number, event: string) {
   const tx = db.transaction(STORE_NAME, 'readwrite')
   const store = tx.objectStore(STORE_NAME)
   await store.delete(makeKey(ownerId, event))
+  db.close()
 }
 
 async function load(ownerId: number, event: string): Promise<string> {
@@ -50,6 +52,7 @@ async function load(ownerId: number, event: string): Promise<string> {
   const tx = db.transaction(STORE_NAME)
   const store = tx.objectStore(STORE_NAME)
   const leaderboard = await store.get(makeKey(ownerId, event))
+  db.close()
   return leaderboard?.data || ""
 }
 
@@ -60,6 +63,7 @@ async function getAll(): Promise<ShortDesc[]> {
   const tx = db.transaction(STORE_NAME)
   const store = tx.objectStore(STORE_NAME)
   const data = await store.getAll()
+  db.close()
   return data.map(({ownerId, owner, event, saveTime}: ShortDesc) => ({ownerId, owner, event, saveTime}))
 }
 
