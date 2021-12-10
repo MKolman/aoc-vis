9fafc2c6a3262f17aa4d681e619a390dde5303d5
Maks Kolman <maks@kolman.si>
2021-12-10 15:54:46 +0100

    chore: move domain from www.kolman.si to aoc.kolman.si

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/aoc-vis/',
+    base: '/',
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
